375b73fb4c10de3842b5416d7aca19d6cffaf1f4
Mateusz Twardowski <twardym78@gmial.comm>
2024-05-04 00:06:01 +0200

    Calculator

--- src/main/java/com/kodilla/kalkulator/Calculator.java
+++ src/main/java/com/kodilla/kalkulator/Calculator.java
@@ -6,13 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Calculator {
 
-	public void someMethod(){
-		System.out.println("It's works!!!");
+	public int add(int a, int b){
+		int c = a + b;
+		return a+b;
+	}
+	public int subtraction(int a, int b){
+		int c = a - b;
+		return c;
 	}
 
 	public static void main(String[] args) {
 			Calculator calculator = new Calculator();
-			calculator.someMethod();
-
+			System.out.println(calculator.add(63, 36));
+			System.out.println(calculator.subtraction(55, 34));
 		}
 }
